a348df4f5b6eef9e58bade779be3ac31afff8c73
xenosign <xenosign@naver.com>
2023-09-08 13:05:02 +0900

    clerk 에러 수정

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
-import "../globla.css";
+import "../globals.css";
 
 export const metadata = {
   title: "Threads",
